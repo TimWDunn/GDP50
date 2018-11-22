d6207bb827adee4f3ea69244f148cb73280c36e5
Tim <twd1g15@soton.ac.uk>
2018-11-22 10:43:23 +0000

    3 Buttons linked to pages.

--- app/src/main/java/com/example/user/gdp50/MainActivity.java
+++ app/src/main/java/com/example/user/gdp50/MainActivity.java
@@ -11,23 +11,52 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        button  = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSecond_Activity();
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openThird_Activity();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFourth_Activity();
+            }
+        });
     }
 
     public void openSecond_Activity() {
         Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void openThird_Activity() {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFourth_Activity() {
+        Intent intent = new Intent(this, FourthActivity.class);
         startActivity(intent);
     }
 }
